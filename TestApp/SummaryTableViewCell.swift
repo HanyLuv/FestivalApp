abaf11044b8d17aac86d9d3b88d869f0b7bf090b
hany <hh.kim@memebox.com>
2017-08-01 19:18:13 +0900

    서머리 뷰

---
+++ TestApp/SummaryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SummaryTableViewCell.swift
+//  TestApp
+//
+//  Created by hany on 2017. 7. 27..
+//  Copyright © 2017년 hany. All rights reserved.
+//
+
+import UIKit
+
+class SummaryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
